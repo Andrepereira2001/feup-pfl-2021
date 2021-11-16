6c3ee2bd056658207f640008154ef3633635368a
André Pereira <adbp@live.com.pt>
2021-11-16 12:29:59 +0000

    modified fib

--- Fib.hs
+++ Fib.hs
@@ -3,13 +3,13 @@ fibRec 0 = 0
 fibRec 1 = 1
 fibRec n = fibRec(n-1) + fibRec(n-2)
 
-fibLista :: Int -> Int
-fibLista n = fibs!!n
+fibLista :: (Integral a) => a -> a
+fibLista n = fibs !! fromIntegral n 
             where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
 
 
-fibListaInfinita :: Int -> Int
-fibListaInfinita n = fibs!!n
+fibListaInfinita :: (Integral a) => a -> a
+fibListaInfinita n = fibs !! fromIntegral n 
                     where fibs = 0 : 1 : zipWith (+) fibs (tail fibs)
 
