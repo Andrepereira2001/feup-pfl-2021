4e70b63e41f81104d260d7034fe753397cb23163
André Pereira <adbp@live.com.pt>
2022-01-20 23:05:11 +0000

    FINAL COMMIT

--- prolog/game.pl
+++ prolog/game.pl
@@ -244,8 +244,7 @@ Parameters:
     2. Type of Player to play (human or pc)
     3. Move to receive
 */
-choose_move([Player | Board], human, [[PieceX,PieceY],[DeltaX,DeltaY]]):- write('again'),
-                                                                          ask_piece(PieceX, PieceY),
+choose_move([Player | Board], human, [[PieceX,PieceY],[DeltaX,DeltaY]]):- ask_piece(PieceX, PieceY),
                                                                           validate_player([Player | Board], [PieceX, PieceY]),
                                                                           ask_move(MoveX, MoveY),
                                                                           DeltaX is MoveX - PieceX,
